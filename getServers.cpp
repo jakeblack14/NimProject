42b6d52c3db91b1371c447d34b65d4228d2d401d
jakeblack14 <ablack3@harding.edu>
2018-04-18 13:47:45 -0500

    Added Files we need

---
+++ getServers.cpp
@@ -0,0 +1,78 @@
+#include "TicTacToe.h"
+#include <WinSock2.h>
+#include <iostream>
+
+// getServers.cpp
+//  Given a socket, a broadcast address and a port number, this function returns the number
+//  of TicTacToe servers as well as an array of structs.  Each struct contains the name, IP_Address and 
+//  port number of a remote TicTacToe server.
+
+//	IN parameters:
+//	SOCKET s				= Allocated UDP socket handle that can be used for communcations
+//	char *broadcastAddress	= The broadcast address that should be used for the current LAN
+//	char *broadcastPort		= The port that should be used for the broadcast
+
+//	OUT parameters:
+//	ServerStruct serverArray[]	= An array that contains the structs.  The members of each struct
+//							  contain the name, IP Address and port number of a remote TicTacToe server.
+//	numServers				= Number of elements in serverArray[]
+
+//	Function return value	= Number of remote TicTacToe servers (size of serverArray[])
+
+int getServers(SOCKET s, char *broadcastAddress, char *broadcastPort, ServerStruct serverArray[])
+{
+	// This function returns the number of TicTacToe servers that may be found within the current Broadcast Domain.
+	// The last parameter, serverArray[], will contain all of the servers' names, IP addresses and port numbers.
+
+	int numServers = 0;
+	char ticTacToe_Query[5] = TicTacToe_QUERY;
+	int numSent = UDP_send(s, ticTacToe_Query, 5, broadcastAddress, broadcastPort);
+
+	// Send TicTacToe_QUERY to broadcastAddress using broadcastPort
+/****			
+Task 3: Add code here that will send the TicTacToe_QUERY message to the broadcastAddress using the broadcastPort (see function header).
+****/
+
+
+	// Receive incoming UDP datagrams (with a maximum of 2 second wait before each UDP_recv() function call
+	// As you read datagrams, if they start with the prefix: TicTacToe_NAME, parse out the server's name
+	// and add the name, host address and port number to serverArray[].  Don't forget to increment numServers.
+	int status = wait(s,2,0);
+	if (status > 0) {
+		int len = 0;
+		char recvBuffer[MAX_RECV_BUF + 1];
+		char host[v4AddressSize];
+		char port[portNumberSize];
+		
+/****			
+Task 4a: Add code here that will receive a response to the broadcast message
+****/		
+		len = UDP_recv(s, recvBuffer, MAX_RECV_BUF-1, host, port);
+
+
+		while (status > 0 && len > 0) {
+/****			
+Task 4b: Inside this while loop, extract a response, which should be a C-string that looks like "Name=some server's name".
+		 If the response doesn't begin with the characters, "Name=", ignore it.
+		 If it does begin with the characters, "Name=", parse the actual name that follows and
+		 (i) assign that name to the array of structs, serverArray[numServers].name
+		 (ii) assign the IP Address from which the response originated to serverArray[numServers].host
+		 (iii) assign the server's port number to serverArray[numServers].port
+		 (iv) increment numServers
+****/
+			if (strncmp("Name=", recvBuffer, 5) == 0) {
+				char*token = strtok(recvBuffer, "=");
+				token = strtok(NULL, "=");
+				serverArray[numServers].name = token;
+				serverArray[numServers].host = host;
+				serverArray[numServers].port = port;
+				numServers++;
+			}
+			// Now, we'll see if there is another response.
+			status = wait(s,2,0);
+			if (status > 0)
+				len = UDP_recv(s, recvBuffer, MAX_RECV_BUF, host, port);
+		}
+	}
+	return numServers;
+}
---
+++ nim.h
@@ -0,0 +1,37 @@
+// nim.h    Header file for Nim project
+#pragma once
+#include <winsock2.h>
+#include <string>
+
+#define NIM_QUERY	"Who?"
+#define NIM_NAME	"Name="
+#define NIM_CHALLENGE	"Player="
+
+static char NIM_UDPPORT[] = "29333";
+
+const int v4AddressSize = 16;
+const int portNumberSize = 10;
+const int WAIT_TIME = 120;
+const int MAX_SEND_BUF = 2048;
+const int MAX_RECV_BUF = 2048;
+const int MAX_SERVERS = 100;
+const int MAX_NAME = 100;
+
+struct ServerStruct {
+	std::string name;
+	std::string host;
+	std::string port;
+};
+
+void playNim(SOCKET, bool, char*, bool, bool, char*, char*, bool);
+SOCKET connectsock(const char*, const char*, const char*);
+SOCKET passivesock(const char*, const char*);
+int UDP_recv(SOCKET, char*, int, char*, char*);
+int UDP_send(SOCKET, char*, int, const char*, const char*);
+int wait(SOCKET, int, int);
+char* timestamp();
+int getServers(SOCKET, char*, char*, ServerStruct[]);
+int playTicTacToe(SOCKET, std::string, std::string, std::string, int);
+int serverMain(int, char *argv[], std::string);
+int clientMain(int, char *argv[], std::string);
+int getIPAddressInfo(char*, char*);
